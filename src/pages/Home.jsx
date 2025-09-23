948b16497dc22ae90145e38b5c890291d0972966
Biman Chandra Das <bimandas1611@gmail.com>
2025-09-23 15:15:55 +0530

    My first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+
+export const Home = () => {
+    return <div className ="min-h-screen bg-background text-forground overflow-x-hidden">
+
+        {/* Theme Toggle  */}
+            <ThemeToggle />
+        {/* Background Animation */}
+
+        
+
+        {/* Navbar */  }
+
+        {/* Main Section */}
+
+
+
+        {/* Footer */}
+
+
+    </div>;
+};
