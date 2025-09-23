8712beb32762f4b5f76ab65119cee9d8f28057fa
Biman Chandra Das <bimandas1611@gmail.com>
2025-09-23 15:49:14 +0530

    adding background starts and meteors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
 
@@ -7,7 +8,7 @@ export const Home = () => {
         {/* Theme Toggle  */}
             <ThemeToggle />
         {/* Background Animation */}
-
+            <StarBackground />
         
 
         {/* Navbar */  }
