9935eb0ac1b90070e21f54fce268dfe21bad2da7
Biman Chandra Das <bimandas1611@gmail.com>
2025-09-28 22:47:52 +0530

    Added new sections like Projects and Skills

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { AboutSection } from "../components/AboutSection";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -19,6 +21,8 @@ export const Home = () => {
         {/* Main Section */}
             <HeroSection />
             <AboutSection />
+            <SkillsSection />
+            <ProjectsSection />
 
 
         {/* Footer */}
